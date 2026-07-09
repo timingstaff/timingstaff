954c74c7c3ce49697300c4837d9762a953df9ed2
timingstaff <timing1354@naver.com>
2026-07-09 18:57:14 +0900

    Update inventory.js

--- js/inventory.js
+++ js/inventory.js
@@ -1,1 +1,30 @@
+const inventoryItems = [
+  {
+    name: "치즈 종류",
+    status: "긴급",
+    color: "red",
+    memo: "확인 필요"
+  },
+  {
+    name: "레몬",
+    status: "확인",
+    color: "yellow",
+    memo: "수량 확인"
+  },
+  {
+    name: "잭다니엘",
+    status: "확인",
+    color: "yellow",
+    memo: "바코드 확인"
+  }
+];
 
+function getUrgentInventoryCount() {
+  return inventoryItems.filter(item => item.color === "red").length;
+}
+
+function getCheckInventoryCount() {
+  return inventoryItems.filter(item =>
+    item.color === "red" || item.color === "yellow"
+  ).length;
+}
