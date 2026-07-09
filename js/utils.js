753b6215220d896c4625d8c6f24ebf9c34729796
timingstaff <timing1354@naver.com>
2026-07-09 18:53:55 +0900

    Update utils.js

--- js/utils.js
+++ js/utils.js
@@ -1,1 +1,19 @@
+function getNowTime() {
+  return new Date().toLocaleTimeString("ko-KR", {
+    hour: "2-digit",
+    minute: "2-digit"
+  });
+}
 
+function setTodayDate() {
+  const days = ["일", "월", "화", "수", "목", "금", "토"];
+  const today = new Date();
+
+  const year = today.getFullYear();
+  const month = String(today.getMonth() + 1).padStart(2, "0");
+  const date = String(today.getDate()).padStart(2, "0");
+  const day = days[today.getDay()];
+
+  document.getElementById("todayDate").textContent =
+    `${year}.${month}.${date} (${day})`;
+}
